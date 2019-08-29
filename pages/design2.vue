c46fc1f90ea1679aa97b79da2353d83a56a98ba3
Eugene Rivera <erivera@rh-er-21434.restorationhardware.com>
2019-08-29 12:35:44 -0700

    v4

--- pages/design2.vue
+++ pages/design2.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="root bg-black">
         <section class="bucket2 reveal">
-            <p class="pb-12" id="bucket2Line1" ref="bucket2Line1">We are not a hotel.</p>
+            <p class="pb-12" id="bucket2Line1" ref="bucket2Line1" style="opacity: 1;">We are not a hotel.</p>
             <p id="bucket2Line2" ref="bucket2Line2">This is our house,</p>
             <p id="bucket2Line3" ref="bucket2Line3">AND you are our guest.</p>
             <p id="bucket2Line4" ref="bucket2Line4">Everything is designed</p>
@@ -18,48 +18,76 @@ import ScrollReveal from 'scrollreveal'
 
 export default {
     mounted() {
-        window.addEventListener('load', () => {
-            ScrollReveal({ reset: true, duration: 1500 })
-            ScrollReveal().reveal('.reveal', {afterReveal: this.highlight, afterReset: this.reset})
-        })
+        // window.addEventListener('load', () => {
+        //     ScrollReveal({ reset: true, duration: 1500 })
+        //     ScrollReveal().reveal('.reveal', {afterReveal: this.highlight, afterReset: this.reset})
+        // })
+        setTimeout(() => {
+            this.$refs.bucket2Line1.style.color = '#363636';
+            this.$refs.bucket2Line2.style.opacity = '1';
+        }, 2000)
+        setTimeout(() => {
+            this.$refs.bucket2Line3.style.opacity = '1';
+        }, 4000)
+        setTimeout(() => {
+            this.$refs.bucket2Line2.style.color = '#363636';
+            this.$refs.bucket2Line3.style.color = '#363636';
+            this.$refs.bucket2Line4.style.opacity = '1';
+        }, 6000)
+        setTimeout(() => {
+            this.$refs.bucket2Line5.style.opacity = '1';
+        }, 8000)
+        setTimeout(() => {
+            this.$refs.bucket2Line4.style.color = '#363636';
+            this.$refs.bucket2Line5.style.color = '#363636';
+        }, 10000)
+        // setTimeout(() => {
+        //     this.$refs.bucket2Line1.style.color = '#363636';
+        //     this.$refs.bucket2Line2.style.color = '#363636';
+        //     this.$refs.bucket2Line3.style.color = '#363636';
+        //     this.$refs.bucket2Line4.style.color = '#363636';
+        //     this.$refs.bucket2Line5.style.color = '#363636';
+
+        //     this.$refs.bucket2Line1.style.opacity = '1';
+        //     this.$refs.bucket2Line2.style.opacity = '1';
+        //     this.$refs.bucket2Line3.style.opacity = '1';
+        //     this.$refs.bucket2Line4.style.opacity = '1';
+        //     this.$refs.bucket2Line5.style.opacity = '1';
+        // }, 11000)
     },
     methods: {
         highlight (el) {
-            this.$refs.bucket2Line1.style.color = 'white';
-            this.$refs.bucket2Line1.style.fontSize = '53.5pt';
+            // this.$refs.bucket2Line1.style.color = 'white';
+            // this.$refs.bucket2Line1.style.fontSize = '53.5pt';
             setTimeout(() => {
-                this.$refs.bucket2Line1.style.fontSize = '53pt';
+                // this.$refs.bucket2Line1.style.fontSize = '53pt';
                 this.$refs.bucket2Line1.style.color = '#363636';
                 this.$refs.bucket2Line2.style.color = 'white';
-                this.$refs.bucket2Line2.style.fontSize = '53.5pt';
+                // this.$refs.bucket2Line2.style.fontSize = '53.5pt';
             }, 2500)
             setTimeout(() => {
                 this.$refs.bucket2Line3.style.color = 'white';
-                this.$refs.bucket2Line3.style.fontSize = '53.5pt';
+                // this.$refs.bucket2Line3.style.fontSize = '53.5pt';
             }, 4500)
             setTimeout(() => {
                 this.$refs.bucket2Line2.style.color = '#363636';
-                this.$refs.bucket2Line2.style.fontSize = '53pt';
+                // this.$refs.bucket2Line2.style.fontSize = '53pt';
                 this.$refs.bucket2Line3.style.color = '#363636';
-                this.$refs.bucket2Line3.style.fontSize = '53pt';
+                // this.$refs.bucket2Line3.style.fontSize = '53pt';
                 this.$refs.bucket2Line4.style.color = 'white';
-                this.$refs.bucket2Line4.style.fontSize = '53.5pt';
+                // this.$refs.bucket2Line4.style.fontSize = '53.5pt';
             }, 7000)
             setTimeout(() => {
                 this.$refs.bucket2Line5.style.color = 'white';
-                this.$refs.bucket2Line5.style.fontSize = '53.5pt';
+                // this.$refs.bucket2Line5.style.fontSize = '53.5pt';
             }, 9500)
             setTimeout(() => {
                 this.$refs.bucket2Line4.style.color = '#363636';
-                this.$refs.bucket2Line4.style.fontSize = '53pt';
+                // this.$refs.bucket2Line4.style.fontSize = '53pt';
                 this.$refs.bucket2Line5.style.color = '#363636';
-                this.$refs.bucket2Line5.style.fontSize = '53pt';
+                // this.$refs.bucket2Line5.style.fontSize = '53pt';
             }, 12000)
-        },
-        // reset (el) {
-        //     this.$refs.bucket2Line4.style.color = '#363636';
-        //     this.$refs.bucket2Line5.style.color = '#363636';
-        // }
+        }
     }
 }
 </script>
@@ -73,13 +101,15 @@ section {
     scroll-snap-align: center;
 }
 p {
-    color: #363636;
+    /* color: #363636; */
+    color: white;
     font-family: 'CaslonRH-Superfine', sans-serif;
     font-size: 53pt;
     line-height: 42pt;
     text-transform: uppercase;
-    transition: color 300ms linear, font-size 300ms linear;
-    -webkit-transition: color 300ms linear, font-size 300ms linear;
+    transition: all 300ms linear;
+    /* -webkit-transition: color 300ms linear, font-size 300ms linear; */
+    opacity: 0;
 }
 .root {
     scroll-snap-type: y mandatory;
@@ -90,5 +120,12 @@ p {
 }
 .bucket2 {
     flex-direction: column;
+}
+#bucket2Line1 {
+    animation: fadeIn 500ms linear;
+}
+@keyframes fadeIn {
+    0% {opacity: 0;}
+    100% {opacity: 1;}
 }
 </style>
